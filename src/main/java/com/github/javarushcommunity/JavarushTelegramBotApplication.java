a9536467da69a16ade501fa754c730a61f97a6cb
RMJ15 <161062942+RMJ15@users.noreply.github.com>
2026-01-05 18:16:02 +0500

    Update JavarushTelegramBotApplication.java
    Добавил импорты в строчке 3 и 4

--- src/main/java/com/github/javarushcommunity/JavarushTelegramBotApplication.java
+++ src/main/java/com/github/javarushcommunity/JavarushTelegramBotApplication.java
@@ -1,5 +1,6 @@
 package com.github.javarushcommunity;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //Простая структура Spring-проекта.
