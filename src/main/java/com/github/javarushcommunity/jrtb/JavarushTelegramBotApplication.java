a73c43f66715a1df2e0af9dfa2d46a0983bc0727
RMJ15 <rmur19814@gmail.com>
2026-01-05 13:50:08 +0500

    JRTB-0: создание проекта Spring Boot на основе шаблона.

--- src/main/java/com/github/javarushcommunity/jrtb/JavarushTelegramBotApplication.java
+++ src/main/java/com/github/javarushcommunity/jrtb/JavarushTelegramBotApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-//Простая структура Spring-проекта
+//Простая структура Spring-проекта.
 @SpringBootApplication
 public class JavarushTelegramBotApplication {
 
