0dabcb302c0282361cc42d8b416598861f1ce52f
RMJ15 <161062942+RMJ15@users.noreply.github.com>
2026-01-05 18:13:01 +0500

    Обновил JavarushTelegramBotApplication.java
    Добавил коментарий на 5 строчке

--- src/main/java/com/github/javarushcommunity/JavarushTelegramBotApplication.java
+++ src/main/java/com/github/javarushcommunity/JavarushTelegramBotApplication.java
@@ -2,6 +2,7 @@ package com.github.javarushcommunity;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Простая структура Spring-проекта.
 @SpringBootApplication
 public class JavarushTelegramBotApplication {
     public static void main(String[] args) {
