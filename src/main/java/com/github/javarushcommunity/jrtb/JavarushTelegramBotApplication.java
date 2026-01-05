4774f5eb75200446bb9da32927d248bbf1607e2b
RMJ15 <rmur19814@gmail.com>
2026-01-05 12:47:00 +0500

    Тестовая отправка коммита

--- src/main/java/com/github/javarushcommunity/jrtb/JavarushTelegramBotApplication.java
+++ src/main/java/com/github/javarushcommunity/jrtb/JavarushTelegramBotApplication.java
@@ -3,6 +3,7 @@ package com.github.javarushcommunity.jrtb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 //Простая структура Spring-проекта
 @SpringBootApplication
 public class JavarushTelegramBotApplication {
