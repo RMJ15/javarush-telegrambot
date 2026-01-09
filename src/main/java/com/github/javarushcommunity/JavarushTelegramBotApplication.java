9973f129b5432463c551aa95b8cddd81100cf410
RMJ15 <rmur19814@gmail.com>
2026-01-09 15:05:37 +0500

    Добавил пробел в строчке 5 и 10

--- src/main/java/com/github/javarushcommunity/JavarushTelegramBotApplication.java
+++ src/main/java/com/github/javarushcommunity/JavarushTelegramBotApplication.java
@@ -3,10 +3,12 @@ package com.github.javarushcommunity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 //Простая структура Spring-проекта.
 @SpringBootApplication
 public class JavarushTelegramBotApplication {
+
     public static void main(String[] args) {
-        System.out.println("Hello,world!");
+        SpringApplication.run(JavarushTelegramBotApplication.class, args);
     }
 }
